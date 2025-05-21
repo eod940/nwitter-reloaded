59a9aa20b391326fe8920bcb179e9475b2fb7fbf
eod940 <eod940@gmail.com>
2025-05-21 21:46:34 +0900

    feat: [main] 버튼 추가 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import {useState} from 'react';
+
 function App() {
+  const [clickCount, setClickCount] = useState(0);
   return (
-    <></>
+    <>
+      <h1>반가워요 eod940입니다.</h1>
+      <a href={'https://github.com/eod940/nwitter-reloaded'}>깃허브입니다.</a>
+      <br/>
+      <br/>
+      <button onClick={() => {
+        if (clickCount < 10) {
+          alert('누르지 말라니깐!!');
+          setClickCount(prev => prev + 1);
+        }
+      }}>누르지 마세요
+      </button>
+    </>
   )
 }
 
